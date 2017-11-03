031f124e19cfe9211cac5e81203b254db95a1fcb
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2017-11-03 17:13:50 -0500

    Add Recipe and Note domain; and link them with One-to-One relationship

---
+++ src/main/java/pongpon/springframework/domain/Notes.java
@@ -0,0 +1,16 @@
+package pongpon.springframework.domain;
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+}
