9a239d65ba0ad528b7b99a51d312423cd61f8d17
Pongpon Tienthong <pongpon.tienthong@gmail.com>
2017-11-14 10:21:08 -0600

    Add Integration test

---
+++ src/test/java/pongpon/springframework/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,33 @@
+package pongpon.springframework.repositories;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pongpon.springframework.domain.UnitOfMeasure;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Test
+    public void findByDescription() throws Exception {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", unitOfMeasureOptional.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() throws Exception {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", unitOfMeasureOptional.get().getDescription());
+    }
+
+}
